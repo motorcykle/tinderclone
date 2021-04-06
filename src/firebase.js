15748af07ee60f37203ba7687b7bab7d9c89bf52
motorcykle <mohamedabdulridha1@gmail.com>
2021-04-06 19:11:36 +0200

    first setup

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzLH9KVE7PQIBQdYbEtsX2maTFHcblx3k",
+  authDomain: "tindercloneting.firebaseapp.com",
+  projectId: "tindercloneting",
+  storageBucket: "tindercloneting.appspot.com",
+  messagingSenderId: "748587504770",
+  appId: "1:748587504770:web:ee4c11b84e3375c0e0909b"
+};
+
